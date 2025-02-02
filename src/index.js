259877c676e5316c6fc35b6941e192da63acd889
Sai Sumanth <104977761+sumanth419@users.noreply.github.com>
2025-02-02 18:15:22 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+var num1, num2, ans;
+
+function add(){
+    num1=parseInt(document.getElementById("number1").value)
+    console.log(num1)
+    num2=parseInt(document.getElementById("number2").value)
+    console.log(num2)
+    ans=num1+num2
+    console.log(ans)
+    document.getElementById("answer").value=ans
+}
+function sub(){
+    num1=parseInt(document.getElementById("number1").value)
+    console.log(num1)
+    num2=parseInt(document.getElementById("number2").value)
+    console.log(num2)
+    ans=num1-num2
+    console.log(ans)
+    document.getElementById("answer").value=ans
+}
