50c38b100c204a50318b478ee589385a54b81b3d
Sai Sumanth <104977761+sumanth419@users.noreply.github.com>
2025-02-03 18:07:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,3 +18,23 @@ function sub(){
     console.log(ans)
     document.getElementById("answer").value=ans
 }
+function mul(){
+    num1=parseInt(document.getElementById("number1").value)
+    console.log(num1)
+    num2=parseInt(document.getElementById("number2").value)
+    console.log(num2)
+    ans=num1*num2
+    console.log(ans)
+    document.getElementById("answer").value=ans
+}
+function div(){
+    num1=parseInt(document.getElementById("number1").value)
+    console.log(num1)
+    num2=parseInt(document.getElementById("number2").value)
+    console.log(num2)
+    ans=num1/num2
+    console.log(ans)
+    document.getElementById("answer").value=ans
+}
+
+    
